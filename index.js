98b052e4d034d37c9461188844c6b69e78b6bac5
jesusvelez <jesus_velez1998@hotmail.com>
2025-02-13 20:32:24 -0400

    Add CORS support, schedule notifications for missing patient tests, and refactor notification endpoints

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {firestoreDatabase} = require("./Database/firebase");
 const express = require("express"); 
 const admin = require("firebase-admin")
+const cron = require("node-cron");
+const cors = require('cors');
 // const serviceAccount = require("./Database/optimal-doc-firebase-adminsdk-fnfzt-4d507fce77.json");
 const {Expo} = require("expo-server-sdk");
 
@@ -14,6 +16,16 @@ let expo = new Expo();
 
 const tokenArray = [];
 
+app.use(cors()); // Enable CORS for all routes
+
+const corsOptions = {
+  origin: 'http://your-frontend-domain.com', // Replace with your frontend domain
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json()) // Uses middleware to parse the JSON
 
 app.listen(port, () => console.log(`its alive on http://localhost:${port}`))
@@ -25,7 +37,7 @@ app.get(`/getAllPatients`, async (req, resp) => {
                     ...doc.data().patientInfo,
                     documentID: doc.id
                 }))
-        console.log(listOfPatientsFromDB)
+        // console.log(listOfPatientsFromDB)
 
         resp.status(200).send(listOfPatientsFromDB)
     }catch (error) {
@@ -38,25 +50,37 @@ app.post('/submitToken', (req, res) => {
     const token = req.body.token;
     if (!token)
         return res.status(401).json({ error: "Token not provided" });
-    tokenArray.push(token);
-    console.log(`token ->  ${token}`)
+    //tokenArray.push(token);
+    // const docRef = await firebase.firestoreDatabase.addDoc(firebase.firestoreDatabase.collection(firebase.firestoreDatabase.db, "Users"), {
+    //     });
+
+    // console.log(`token ->  ${token}`)
     return res.json({ msg: "Token received successfully"});
 })
 
-app.post("/sendNotification", async (req, res) => {
+app.post("/sendPatientCompletedNotification", async (req, res) => {
     // Create the messages array
-    let token = "ExponentPushToken[oP6arUElkynB5lnYe7uCnA]" 
+    // let token = "ExponentPushToken[wOX2vLBx-1QT7gLI3YNG3i]" 
+    const tokens = await getListOfTokensFromDB();
     let messages = [];
-    if (Expo.isExpoPushToken(token)) {
-        messages.push({
-            to: token,
-            sound: 'default',
-            title: "Gama es mujer",
-            body: "Mas nena no puede ser. Yo creo que le busco un novio",
-            data: { withSome: 'data' },
-        });
-    }
-
+    const {patientRecordNumber} = req.body;
+    const currentTime = new Date().toLocaleString();
+    // console.log("current time -> ", currentTime)
+    // console.log("tokens -> ", tokens)
+    tokens.map(token => {
+        if (Expo.isExpoPushToken(token)) {
+            messages.push({
+                to: token,
+                sound: 'default',
+                title: "Patient Has Completed Test",
+                body: `Patient with record id "${patientRecordNumber}" has completed their test!`,
+                data: { currentTime },
+            });
+        }
+    })
+    if(!patientRecordNumber)
+        return res.status(400).send("Patient record number is required");
+    
     // Send the notifications
     let chunks = expo.chunkPushNotifications(messages);
     let tickets = [];
@@ -74,63 +98,125 @@ app.post("/sendNotification", async (req, res) => {
     res.status(200).send("Notification sent");
 });
 
+async function getListOfTokensFromDB (){
+ const querySnapshot = await firestoreDatabase.getDocs(firestoreDatabase.collection(firestoreDatabase.db, "Users"));
+        const listOfUsersInDB = querySnapshot.docs.map(doc => ({
+                    ...doc.data().userInfo,
+                    documentID: doc.id
+                }))
+        // console.log(listOfUsersInDB)
+    let tokens = listOfUsersInDB.map(user => user.notificationPushToken);
+
+    return tokens;
+}
+
+// Receives the list of patients in the DB that have not completed their tests
+async function notifyDoctorsOfPatientsMissingTests(listOfPatientWithMissingTests) {
+  // Fetch the list of tokens from the database
+  const tokens = await getListOfTokensFromDB();
+
+  let stringOfMissingPatients = "";
+  // Create a string of patient record numbers
+  for (let patient of listOfPatientWithMissingTests) 
+    stringOfMissingPatients += "- " + patient.patientRecordNumber + " \n";
+  
+  
+  let messages = []
+  tokens.map(token => {
+    if (!Expo.isExpoPushToken(token)) {
+      console.error("Invalid Expo push token:", token);
+      //return;
+    }else{
+      messages.push({
+        to: token,
+        sound: "default",
+        title: "Patients with missing tests",
+        body: `The following patients have not yet completed their tests: \n ${stringOfMissingPatients} \n Please remind the patients that they need to complete their tests.`,
+        data: {},
+      });
+    }
+})
+
+// Send the notifications
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  (async () => {
+        for (let chunk of chunks) {
+            try {
+                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                tickets.push(...ticketChunk);
+            } catch (error) {
+                console.error("Error sending notifications to patients -> ", error);
+            }
+        }
+    })();
+
+}
+
+// Function to determine whether a patient has not completed their tests
+function isPatientMissingTests(patient) {
+  return patient.allTestsCompleted === "no";
+}
+
+
+// Function to check if patients are missing their tests and send notifications to the doctors letting them know of the patients that are missing tests
+async function checkPatientsAndSendNotifications(){
+  const currentTime = new Date().toLocaleString();
+  console.log(`Running scheduled patient test check at ${currentTime}`);
+  try {
+    // Fetch patients from Firestore
+    const querySnapshot = await firestoreDatabase.getDocs(firestoreDatabase.collection(firestoreDatabase.db, "Patients"));
+
+    // Map the results to an array of patient objects
+    const patients = querySnapshot.docs.map(doc => ({
+      ...doc.data().patientInfo,
+      documentID: doc.id
+    }));
+    // Send the notifications to the doctors about their patients missing tests
+    await notifyDoctorsOfPatientsMissingTests(patients.filter(patient => isPatientMissingTests(patient)));
+
+  } catch (error) {
+    console.error("Error during scheduled test check:", error);
+  }
+}
+
+
+// Helper function to check if the current day is a multiple of five
+function isMultipleOfFive() {
+  const today = new Date();
+  const dayOfMonth = today.getDate();
+  return dayOfMonth % 5 === 0;
+}
+
+// Schedule a job to run every day at 8:00 AM (server time)
+cron.schedule("0 8 * * *", () =>{
+  if(isMultipleOfFive())
+      checkPatientsAndSendNotifications()
+});
+
+// Schedule a job to run every day at 5:00 PM (server time)
+cron.schedule("0 17 * * *", () =>{
+  if(true)
+      checkPatientsAndSendNotifications()
+});
 
 
 
-// app.get("/sendNotification", (req, res) => {
-//     if (!tokenArray. length) 
-//         return res. status (400). json({ msg: "Fail" })
 
-//     const message = {
-//         notification: {
-//             title: "Your Notification Title", 
-//             body: "Your Notification Body"
-//         },
-//             token: tokenArray[0]
-//     };
-//     admin.messaging().send(message)
-//          .then((response) => {
-//             console.log("Successfully sent message:", response); 
-//             return res.status(200).json({ msg: "Send token" });
-//         })
-//         .catch ((error) =>
-//         {
-//             console.error("Error sending message:", error);
-//             return res.status (400).json({ error });
-//         });
-// })
+// Default error handling middleware that will show 404 when a route is not found
+app.use ((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next (error) ;
+});
 
-// app.post("/submitToken", (req, res) => {
-//     // Access the token from the request body, query parameters, or headers
-//     const token = req.body.token;
-//     // Check if the token is present
-//     if (!token)
-//         return res.status(401).json({ error: "Token not provided" });
-//     tokenArray.push(token);
-//     console.log(`token ->  ${token}`)
-//     // Your logic with the token
-//     // For example, you can send a response indicating success
-//     return res.json({ msg: "Token received successfully"});
-// })
-
-
-// app.get("/getTokens", (req, res) => {
-//     console.log(tokenArray)
-//     res.status(200).send(tokenArray)
-// })
-
-
-// app.use ((req, res, next) => {
-//     const error = new Error("Not found");
-//     error.status = 404;
-//     next (error) ;
-// });
-
-// app.use((error, req, res, next) => {
-//     res.status (error.status || 500);
-//     res. json ({
-//     error: {
-//         message: error.message,
-//     },
-//     });
-// });
+// Error handling middleware that will show the error with a status code
+app.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status (error.status || 500);
+    res. json ({
+    error: {
+        message: error.message,
+    },
+    });
+});
